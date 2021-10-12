6ff41dea14618264d6731540f108c2878ff6b371
sean-mahan <seanmahansean@gmail.com>
2021-10-11 19:51:05 -0500

    last middleware added

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -62,7 +62,11 @@ async function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next()
+  if(!req.body.password || req.body.password.length < 3){
+    next({status: 422, message: "Password must be longer than 3 chars"})
+  }else{
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
